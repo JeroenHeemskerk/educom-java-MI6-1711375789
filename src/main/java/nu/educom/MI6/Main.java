26f79c2c1c6837161f588e7468b8a4128b2ec7df
MilanLucas <MilanLucas2001@outlook.com>
2024-03-26 10:56:29 +0100

    GUI #3

--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -1,30 +1,28 @@
 package nu.educom.MI6;
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Scanner;
-
-import static java.lang.Integer.parseInt;
 
 public class Main {
   public static void main(String[] args) {
     List<String> blackList = new ArrayList<>();
+    JFrame frame = new JFrame("InputDialog Example #1");
     while (blackList.size() != 956){
-      String userNum = userNumber(blackList);
-      boolean login = userLogin();
+      String userNum = userNumber(blackList, frame);
+      boolean login = userLogin(frame);
       if (login) {
-        System.out.println("Welcome " + userNum);
+        JOptionPane.showMessageDialog(frame, "Welcome " + userNum);
       } else {
+        JOptionPane.showMessageDialog(frame, "ACCESS DENIED", "ACCESS DENIED", JOptionPane.WARNING_MESSAGE);
         blackList.add(userNum);
       }
     }
   }
 
-  public static boolean userLogin(){
+  public static boolean userLogin(JFrame frame){
     boolean login = false;
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter code");
-    String code = myObj.nextLine();
+    String code = JOptionPane.showInputDialog(frame, "Enter passphrase");
     if (Objects.equals(code, "For ThE Royal QUEEN")){
       login = true;
     }
@@ -38,8 +36,7 @@ public class Main {
     return userNum;
   }
 
-  public static String userNumber(List<String> blackList) {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+  public static String userNumber(List<String> blackList, JFrame frame) {
     String userNum = "";
     //Need to make an array that contains 001 to 956
     List<String> numbers = new ArrayList<>();
@@ -48,8 +45,7 @@ public class Main {
     }
 
     while (!numbers.contains(userNum) || blackList.contains(userNum)) {
-      System.out.println("Enter service number:");
-      userNum = myObj.nextLine();
+      userNum = JOptionPane.showInputDialog(frame, "Enter servicenumber");
       userNum = bond(userNum);
     }
 
